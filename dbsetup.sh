fddc86efd6beb57b6e0a56c21f42b2c38c1ba694
patocox <patrickocox@gmail.com>
2014-08-11 23:24:16 -0500

    Added sleeps
    Needed to add sleeps since kill were coming too quickly for the processes

--- dbsetup.sh
+++ dbsetup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+sleep 5
 kill $(ps aux | grep '[s]upervisord' | awk '{print $2}')
+sleep 5
 /app/init app:rake gitlab:setup force=yes
+sleep 5
 kill $(ps aux | grep '[s]upervisord' | awk '{print $2}')
+sleep 5
 /app/init app:start
 
